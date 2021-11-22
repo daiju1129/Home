83e5af3fa04baf5ceb319d7dd3ba37d0034d4abd
daiju1129 <daijusoft@i.softbank.jp>
2021-11-22 15:50:48 +0900

    トーク画面できました

---
+++ app/src/main/java/com/example/home/talk.java
@@ -0,0 +1,14 @@
+package com.example.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class talk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_talk);
+    }
+}
